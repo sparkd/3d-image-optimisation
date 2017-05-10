5a1878d5b4bbaef244f3c88c462426e24ef47a51
benscott <ben@benscott.co.uk>
2017-05-10 12:22:42 +0100

    Add crop options

--- optimise.py
+++ optimise.py
@@ -5,14 +5,6 @@ from imageoptim import ImageOptimAPI
 dir = os.path.abspath(os.path.dirname(__file__))
 
 
-FILES = [
-  'PANO_20140705_mars1.jpg',
-  'PANO_20150101_stars.jpg',
-  # 'PANO_Dione_moon_color.jpg',
-  # 'PANO_Enceladus2_ps.jpg'
-]
-
-
 def main():
   api = ImageOptimAPI('zwwpvcxnbr');
   src_dir = os.path.join(dir, 'src');
@@ -22,11 +14,15 @@ def main():
       'output': os.path.join(dir, 'output', fn)
     }
     # If file does not exist, resize it
-    if(os.path.isfile(file_path['src'])):
+    if(os.path.isfile(file_path['output'])):
       print('File %s already exists - skipping' % fn)
     else:
       print('Optimising %s' % fn)
-      img = api.image_from_file_path(file_path['src'], {'size': '4096x2048'})
+      options={
+        'crop': True,
+        # 'quality': 'high'
+      }
+      img = api.image_from_file_path(file_path['src'], options=options, resize_pow2=True)
       img.save(os.path.join(dir, 'output', fn))
 
 
